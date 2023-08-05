4b453962fb397ce84a2e5065c8febdfea551f3d7
Hafid Pradipta <pradipta.hafid@gmail.com>
2023-08-05 16:25:03 +0800

    first commit

---
+++ src/main/java/DemoApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/LoginRegist/LoginRegistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoginRegist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
